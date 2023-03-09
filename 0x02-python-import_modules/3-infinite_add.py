4459cc94af4896bb867064a39995991b3bcaad84
Javan <javan.o.otieno@gmail.com>
2023-03-09 16:52:18 +0300

    Infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    l = [int(x) for x in sys.argv[1:]]
-    print("{}".format(sum(l)))
+    arr = [int(x) for x in sys.argv[1:]]
+    print("{}".format(sum(arr)))
