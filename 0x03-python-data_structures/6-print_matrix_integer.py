9c39e9f0cf2a856cb7e48710ba0fd537771f1dc0
Javan <javan.o.otieno@gmail.com>
2023-03-12 22:31:55 +0300

    Lists of lists = Matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if len(matrix) == 1 and len(matrix[0]) == 0:
+    try:
+        for i, _ in enumerate(matrix):
+            c = 0
+            for j, v in enumerate(matrix[i][:-1]):
+                print("{:d}".format(v), end=" ")
+                c = j
+            print("{:d}".format(matrix[i][c+1]))
+    except IndexError:
         print("")
-        return
-    for i, _ in enumerate(matrix):
-        c = 0
-        for j, v in enumerate(matrix[i][:-1]):
-            print("{:d}".format(v), end=" ")
-            c = j
-        print("{:d}".format(matrix[i][c+1]))
