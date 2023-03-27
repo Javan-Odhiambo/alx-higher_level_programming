0cc90495a48154c9d0c975ae388adb50d7665442
Javan <javan.o.otieno@gmail.com>
2023-03-27 18:49:48 +0300

    Update

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -2,10 +2,9 @@
 import sys
 
 
-def safe_print_integer_err(value):
+def safe_function(fct, *args):
     try:
-        print("{:d}".format(value))
+        return fct(*args)
     except Exception as e:
         print("Exception: {}".format(e), file=sys.stderr)
-        return False
-    return True
+        return None
