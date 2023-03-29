7df7dd64936001e9a21e6c91e23837eb0ff295ea
Javan <javan.o.otieno@gmail.com>
2023-03-29 20:41:48 +0300

    Update

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+class Square:
+	"""Defines a square
+	Attributes:
+		__size (int): size of a side of the square.
+ 	"""
+
+	def __init__(self, size=0):
+		"""Initializes the class
+			Args:
+				size (int): size to initialize default is zero.
+            Returns:
+            	None
+		"""
+
+		if (type(size) != int):
+			raise TypeError("size must be an integer")
+
+		if (size < 0):
+			raise ValueError("size must be >= 0")
+
+		self.__size = size
