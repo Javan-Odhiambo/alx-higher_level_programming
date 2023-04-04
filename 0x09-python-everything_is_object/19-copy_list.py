edb31f645fd17e148ebc227dcee8174c4c4aacbf
Javan <javan.o.otieno@gmail.com>
2023-04-04 23:55:50 +0300

    Everything is an object

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def copy_list(lst):
     """ Creates a copy of of a list given as an argument.
+
         Args:
-            l (list): List to be copied.
+            lst (list): List to be copied.
         Returns:
             (list): Copy of the list l.
     """
