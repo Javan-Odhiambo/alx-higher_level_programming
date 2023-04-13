9836156084223e189fb00d54ee4c9743a35bc211
Javan <javan.o.otieno@gmail.com>
2023-04-13 22:16:58 +0300

    Same Object

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""Contains the function is_same_class"""
+
 
 def is_same_class(obj, a_class):
     """Checks if an object is an instance of a class
@@ -6,7 +8,7 @@ def is_same_class(obj, a_class):
             obj (Any): The object
             a_class (Any): The class to be checked
         Return:
-            True (bool): if object obj is an instance of class a_class  
+            True (bool): if object obj is an instance of class a_class
             False (bool): if object obj is not an instance of class a_class
     """
     return isinstance(obj, a_class)
