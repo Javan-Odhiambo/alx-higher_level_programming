229328cc3d273becd5dbc8b46764304ff813920e
Javan <javan.o.otieno@gmail.com>
2023-04-13 23:18:52 +0300

    Added method area

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Contains class BaseGeometry"""
+
+
+class BaseGeometry:
+    """Defines a class BaseGeometry"""
+
+    def area(self):
+        """Raises execption"""
+        raise Exception("area() is not implemented")
