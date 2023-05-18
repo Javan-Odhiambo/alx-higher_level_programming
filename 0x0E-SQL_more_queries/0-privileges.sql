de973c69d960ad6370d020b121378a20e414f0bd
Javan <javan.o.otieno@gmail.com>
2023-05-18 03:14:25 +0300

    Complete

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 --shows all permissions granted to a user on localhost
-SHOW GRANTS FOR 'user_0d_1';
-SHOW GRANTS FOR 'user_0d_2';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
