3b460819c83b1c3cf6667239b5e63a972bf56746
Javan <javan.o.otieno@gmail.com>
2023-07-01 05:31:08 +0300

    Initial solution

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# takes in a URL, sends a request to that URL, and displays the size of the body of the response
+
+curl -sI $1 | grep content-length | awk '{print $2}'
---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# script that takes in a URL, sends a GET request to the URL, and displays the body of the response
+
+if [ $(curl -sI $1 | head -n 1| awk '{print $2}') -eq 200 ]; then
+  curl -s $1
+fi
